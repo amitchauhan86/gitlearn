a17ccf98f434db2c5f9db013b2cd81e81e6ad2f1
Amit chauhan <amit@cwebconsultants.com>
2024-11-19 16:24:45 +0530

    aa

--- src/App.js
+++ src/App.js
@@ -5,7 +5,10 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import MainScreen from './Screens/MainScreen';
 
 function AppContent() {
-  console.log("this is change only master branch !")
+  console.log("this is change only dev branch4")
+  console.log("this is change only dev branch1")
+  console.log("this is change only dev branch2")
+  console.log("this is change only dev branch3")
   return (
     <Routes>
       <Route exact path="/main" element={<MainPanel />} />
