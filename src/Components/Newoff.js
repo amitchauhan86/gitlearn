406dca9a037502d7c6d11ffc5e8ed0f49066d015
Amit chauhan <amit@cwebconsultants.com>
2024-11-19 18:01:03 +0530

    new off

---
+++ src/Components/Newoff.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Newoff = props => {
+  return (
+    <div>Newoff</div>
+  )
+}
+
+Newoff.propTypes = {}
+
+export default Newoff
